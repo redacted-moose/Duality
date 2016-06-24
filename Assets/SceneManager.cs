dbe55ac05ec8717ff7069caf5f592f9c379544db
Lojalist <rnlabor@NBW-09.informatik.hs-ulm.de>
2016-06-24 10:41:32 +0200

    CSharp Scripts

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+    public Transform mainMenu, optionsMenu;
+
+    public void LoadScene(string name)
+    {
+        
+        Application.LoadLevel(name);
+        
+        
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsMenu(bool clicked)
+    {
+        if(clicked == true)
+        {
+            optionsMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(false);
+
+        }else
+        {
+            optionsMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(true);
+
+        }
+    }
+}
+
