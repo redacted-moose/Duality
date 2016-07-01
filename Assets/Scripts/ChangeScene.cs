e958fa4fb88d721a8f2f476df80add74b4464a20
Chris Sadler <sadlercr@rose-hulman.edu>
2016-07-01 14:28:12 +0200

    Rudimentary teleportation works!

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,20 +1,38 @@
-﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
+
+public enum DualityScene {
+	Cave = 0,
+	Forest = 1,
+	Village = 2
+}
 
 public class ChangeScene : MonoBehaviour {
 
+	public DualityScene target;
+	public float x;
+	public float y;
+	public float z;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
 	}
 
-    void OnTriggerEnter()
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnTriggerEnter(Collider other)
     {
-        Application.LoadLevel("test");
+		// other.gameObject.GetComponent<Transform>().position = new Vector3(x, y, z);
+		GameObject player = other.gameObject;
+        SceneManager.LoadScene((int)target);
+		if (player.tag == "Player") {
+			Transform t = player.GetComponent<Transform>().transform;
+			t.position = new Vector3(x, y, z);
+		}
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	void Awake() {
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
